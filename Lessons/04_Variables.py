bc950009d75a1549878e6a16ca649db4f445a1d1
hilel <hilel941343@gmail.comm>
2025-03-05 14:12:47 +0200

    Lesson4

---
+++ Lessons/04_Variables.py
@@ -0,0 +1,42 @@
+#בפייתון יש ייחודיות משאר שפות התוכנה כך
+# שכאשר נגדיר טייפ למשתנה שלנו תמיד אפשר לשנות את הטייפ 
+
+name = "Hillel"
+print(name)
+
+# name = 35
+# print(name)
+# name = True
+# print(name)
+
+# types חיסרון גדול של השיטה הזו שזה גורם לבלבול ולא מקצועי כי יכול לגרם לשגיאות וערבוב בין 
+age = 35 
+print(age + 1.58)
+print(age + 1)
+print(age -1)
+print("Hello," + name )
+print(name * 5)
+# age = "Hillel"
+# print(age + 1)
+
+# price = 45.99
+# 
+# is_admin = False
+
+#החלפת אות מקטנה לגדולה או ההיפך מייצרת שני משתנים שונים לגמרי
+
+# my_lovely_country_2025= "Israel"
+# my_lovely_Country_2025= "Israel2025"
+# print(my_lovely_country_2025)
+# print(my_lovely_Country_2025)
+
+#עדיף לשמור במשתנים כך שכאשר יצטרך להתבצע שינוי בכתובת יהיה צורך לשנות רק את המשתנה עצמוUrl's 
+# url = print("https://www.google.co.il")
+# print(url)
+# print(url)
+# print(url)
+# print(url)
+# print(url)
+# print(url)
+# print(url)
+
