190b215db2c50a7c6cbf5b87c6b505d8b99121d4
hilel <hilel941343@gmail.comm>
2025-03-05 10:44:43 +0200

    lesson1

--- Lessons/Comments.py
+++ Lessons/Comments.py
@@ -1,1 +1,11 @@
 #In this first lesson we will learn how to write a comment in python laguage.
+#Printing my name 
+print('hello, Alex')
+
+
+
+
+
+#multi line commment = in win Ctrl + / ,in Mac Cmd + / 
+# print(123)py
+# print(False)
