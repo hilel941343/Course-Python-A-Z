a546dd338542c7f90c014c1523904aa78a123ebf
hilel <hilel941343@gmail.comm>
2025-03-05 15:15:36 +0200

    Lesson7

---
+++ Lessons/07_booleans_comparison.py
@@ -0,0 +1,13 @@
+#booleans True /False
+# is_true = False
+# print(is_true)
+# print(type(is_true))
+
+# Comparison operators: > / < / <= / >= /== / != 
+# print(5 == 4)
+#print("Hillel" < "hillel")
+
+# 65 == 97
+print("A" == "a")
+print(ord("A"))
+print(ord("a"))
