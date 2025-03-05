7936d4cd4f692df089b7fd2f5625203b20c69a0d
hilel <hilel941343@gmail.comm>
2025-03-05 14:33:47 +0200

    Lesson5

---
+++ Lessons/05_F_string.py
@@ -0,0 +1,12 @@
+name = "Hillel"
+greeting = "Hello"
+age =24
+print(greeting + " " + name)
+print(f"{greeting} {name}")
+print(F"{greeting} {name}")
+print(f'{greeting} {name}')
+print(F'{greeting} {name}')    
+print("my name is " + name + ". my age is " + str(age))
+#הדרך הכי פשוטה ויעילה לשילוב בין  סוגים שונים של משתנים
+print(f"my name is {name}. my age is {age}") 
+print(f"my name is {name.upper()}. Next year - my age is  going to be {age+1}") 
