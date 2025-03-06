0b196b79cc17be1a468696806375efbb8f5856b1
hilel <hilel941343@gmail.comm>
2025-03-06 11:39:42 +0200

    Lesson16

---
+++ Lessons/16_match_case.py
@@ -0,0 +1,24 @@
+value = 11.5
+#טייפ  משייך את הקלאסיפיקציה של המשתנה 
+if type(value) is str :
+    print("The value is string")
+elif type(value) is int or type(value) is float:
+    print("The value is number")
+elif type(value) is bool:
+    print("The value is boolean")
+elif type(value) is list:
+    print("The value is list")
+else:
+    print("The value is unkown type!")
+#__________________________________________________________________________________________
+match type(value):
+    case __builtins__.str:
+         print("The value is string")
+    case __builtins__.int | __builtins__.float:
+         print("The value is number")
+    case __builtins__.bool:
+        print("The value is boolean")
+    case __builtins__.list:
+        print("The value is list")
+    case _:
+        print("The value is unkown type!")
