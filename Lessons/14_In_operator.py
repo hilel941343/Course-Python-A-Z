fa76d3cca62cea9238c6abd93c13958e4b7f279f
hilel <hilel941343@gmail.comm>
2025-03-06 11:09:59 +0200

    Lesson14

---
+++ Lessons/14_In_operator.py
@@ -0,0 +1,17 @@
+# # value = "Hello World!"
+# # value = ["Hello","World!","Alex"]
+# value = ("Hello","World!","Alex")
+value =" Hello World Hello Hillel Hello All"
+
+# if "hi" not in value:
+#     print("value exists")
+# else:
+#     print("Value doesn't exist")
+
+if value.count("Hello") > 0:
+    print("value exists")
+else:
+    print("Value doesn't exist")
+
+    
+print(value.count("Hello"))
