7f818e90c09dc8a5f6e251a6660023cbaf4cfc6c
hilel <hilel941343@gmail.comm>
2025-03-06 09:34:06 +0200

    Lesson11

---
+++ Lessons/11_Lists.py
@@ -0,0 +1,28 @@
+#ברשימה בפייתון אפשר לערבב כמה סוגי משתנים 
+#         -6   -5     -4     -3    -2      -1            
+#INDEX:    0  1        2      3    4       5  
+my_list = [1, True, [3,4,10], 5, "Hillel", 7]
+print(my_list.index(5))
+# print(my_list)
+# print(my_list[1])
+# print(my_list[0:5])
+# print(my_list[0:5:2])
+# print(my_list[-4:-1])
+# print(my_list[-4:])
+# print(my_list[::-1])
+# print(my_list[2][2])
+# print(my_list)
+# # print(my_list.pop(2))
+# print(my_list.count("Hillel"))
+# my_list.append("Touitou")
+# my_list += 53,53
+# my_list.reverse()
+my_list =[10,5,4,1,1000]
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list.sort(reverse = True)
+print(my_list)
+
+#:lists אתר ללמידה על 
+# https://www.w3schools.com/python/python_lists_methods.asp
