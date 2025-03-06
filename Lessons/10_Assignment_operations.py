4c26f49594c22e26c22b31c4dd7dc90c572d9c25
hilel <hilel941343@gmail.comm>
2025-03-06 09:01:23 +0200

    Lesson10

---
+++ Lessons/10_Assignment_operations.py
@@ -0,0 +1,38 @@
+# print(5 + 5)
+
+# number = 5
+# print(number)
+# number += 5 # number = number +5 
+# print(number)
+
+# number = 10
+# print(number)
+# number -= 5# number = number -5
+# print(number)
+
+# number = 10
+# print(number)
+# number *= 5# number = number * 5
+# print(number)
+
+
+number = 100
+# print(number)
+# number **= 4 # number = number ** 4
+# print(number)
+
+number //= 4 # number = number // 4
+print(number)
+number /= 4 # number = number / 4
+print(number)
+number %= 4 # number = number % 4
+print(number)
+
+
+
+# print(10 - 5)
+# print(10 * 5)
+# print(3 ** 4) # ** חזקה
+# print(10 / 3) # / חילוק עם שארית
+# print(18 // 4) #// חילוק ללא שארית מחזיר מספר שלם 
+# print(18 % 4) # % מחזיר את השארית  של החילוק
